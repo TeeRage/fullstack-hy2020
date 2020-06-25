0deaf44904557db8b78d644106efb306a5ecb935
Tea Antila <teaa@uef.fi>
2020-06-25 12:49:05 +0300

    kurssitiedot step5

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.olio.name}</h1>
     </div>
   )
 }
@@ -12,9 +12,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part osa={props.parts[0].name} harjoituksia={props.parts[0].exercises}/>
-      <Part osa={props.parts[1].name} harjoituksia={props.parts[1].exercises}/>
-      <Part osa={props.parts[2].name} harjoituksia={props.parts[2].exercises}/>
+      <Part osa={props.olio.parts[0].name} harjoituksia={props.olio.parts[0].exercises}/>
+      <Part osa={props.olio.parts[1].name} harjoituksia={props.olio.parts[1].exercises}/>
+      <Part osa={props.olio.parts[2].name} harjoituksia={props.olio.parts[2].exercises}/>
     </div>
   )
 }
@@ -30,35 +30,38 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p>Number of exercises {props.olio.parts[0].exercises + props.olio.parts[1].exercises + props.olio.parts[2].exercises}</p>
     </div>
   )
 }
 
 const App = () => {
 
-  const course = 'Half Stack application development'
+  const course = {
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+    name: 'Half Stack application development',
+
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {parts} />
-      <Total parts = {parts} />
+      <Header olio = {course}/>
+      <Content olio = {course} />
+      <Total olio = {course} />
     </div>
   )
 }
