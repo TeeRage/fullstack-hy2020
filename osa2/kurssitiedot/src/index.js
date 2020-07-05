e1b2ad4497eb9a68f87856bcefba41fd2f189889
Tea Antila <teaa@uef.fi>
2020-07-05 17:12:46 +0300

    2.4: kurssitiedot step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 //Yhteenlaskettu tehtävien lukumäärä
 const Total = ({ course }) => {
 
+  console.log("Total", course)
+
   //Reducer, luo yhden muuttujan kaikista arrayn muuttujista (summa tässä tapauksessa)
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
@@ -14,13 +16,13 @@ const Total = ({ course }) => {
 
   //Käytetään uutta arrayta ja reduceria summan näyttämäiseen
   return(
-    <h4>Tehtäviä yhteensä {tehtavat.reduce(reducer)}</h4>
-  ) 
+    <h4>Tehtäviä yhteensä {tehtavat.reduce(reducer)}<br/></h4>
+  )
 }
-
 
 //Kurssin sisällön renderöinti (saa propsit Content-komponentilta)
 const Part = (props) => {
+  console.log("Part", props)
   return (
     <div> 
       {props.kurssi.parts.map(kurssi => <p key = {kurssi.id}> {kurssi.name} {kurssi.exercises}</p>)}
@@ -30,6 +32,7 @@ const Part = (props) => {
 
 //Kurssin sisällön renderöinti (saa propsit Course-komponentilta)
 const Content = ({ course }) => {
+  console.log("Content", course)
   return (
     <div> 
       <Part kurssi={course}/>
@@ -39,17 +42,23 @@ const Content = ({ course }) => {
 
 //Kurssin nimen renderöinti (saa propsit Course-komponentilta)
 const Header = ({ course }) => {
+  
+  console.log("Header", course)
+
   return (
-    <h1>{course.name}</h1>
+    <h3>{course.name}</h3>
   )
 }
 
 //yksittäisen kurssin muotoilusta huolehtiva komponentti (saa propsit App-komponentilta)
 const Course = ({ course }) => {
+
+console.log("Course", course)
+
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
+      <Header course = {course} />
+      <Content course = {course} />
       <Total course = {course}/>
     </div>
   )
@@ -57,38 +66,62 @@ const Course = ({ course }) => {
 
 const App = () => {
 
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Tietorakenteet ja algoritmit',
-        exercises: 12,
-        id: 3
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
+  //Mapataan kaikki kurssit arrayksi  
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map((kurssi, id) => {
+        return(<Course key={id} course={kurssi}/>)
+      })}
     </div>
   )
 }
+
+//{kurssiLista.forEach(element => <Course course = {element}/>)}
 
 ReactDOM.render(<App />, document.getElementById('root'))
