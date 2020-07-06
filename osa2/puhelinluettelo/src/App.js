88ab4296bd92b6a650b964de278b9d7c0c50635a
Tea Antila <teaa@uef.fi>
2020-07-06 15:21:24 +0300

    2.9*: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,9 +8,15 @@ const Person = ({person }) => {
 
 const App = () => {
 
-  const [ persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040 123 456' }]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState('')
 
   //Lisätään henkilö persons-listaan
   const addPerson = (event) => {
@@ -43,9 +49,26 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  //Asetetaan uusi numero newNumber muuttujalle
+  const handleSearch = (event) => {    
+    setNewSearch(event.target.value)
+    console.log(newSearch)
+  }
+
+  //Filtteröidään näytettävät nimet haun perusteella
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        Filter shown with: <input
+          value={newSearch}
+          onChange={handleSearch}
+        />
+      </form>
+
+      <h2>Add a new</h2>
       <form onSubmit = {addPerson}>
         <div>
           name: <input 
@@ -65,7 +88,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((nimi, i) =><Person key ={i} person={nimi}/>)}
+      {personsToShow.map((nimi, i) =><Person key ={i} person={nimi}/>)}
     </div>
   )
 }
