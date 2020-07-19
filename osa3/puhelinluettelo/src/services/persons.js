92ba37d18ef012085e18c6541311c63bc9b2dfc7
Tea Antila <teaa@uef.fi>
2020-07-19 14:34:18 +0300

    3.11 puhelinluettelo full stack

--- osa3/puhelinluettelo/src/services/persons.js
+++ osa3/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-//const baseUrl = '/api/persons'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
+//const baseUrl = 'http://localhost:3001/api/persons'
+//const baseUrl = 'https://calm-savannah-45350.herokuapp.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
