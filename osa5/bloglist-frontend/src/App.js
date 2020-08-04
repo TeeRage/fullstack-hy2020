63a0610c7c33f2d24437a3fb131a22e7ea85d44e
Tea Antila <teaa@uef.fi>
2020-08-04 13:34:31 +0300

    5.1: blogilistan frontend, step1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,22 +1,92 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
 
+  //Haetaan blogit tietokannasta
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  //Kirjautumispainikkeen toiminto
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  //Sivun ulkoasu, kun käyttäjä on kirjautunut onnistuneesti sisään
+  const blogForm = () => (
+    <div>
+     <ul>
+       {blogs.map((blog, i) => 
+         <Blog
+           key={i}
+           blog={blog}
+         />
+       )}
+     </ul>
+    </div>
+  )
+
+  //Sivun ulkoasu, jos käyttäjä ei ole vielä kirjautunut onnistuneesti
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  //Sivun ulkoasu, näytettävä sisältö sen mukaan, onko käyttäjä kirjautunut vai ei
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
+      <h1>Blogs</h1>
+      
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {blogForm()}
+        </div>
+      }
+
     </div>
   )
 }
