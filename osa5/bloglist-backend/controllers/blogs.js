607c7fe892456a2e0972f5b6b0d18762f46fb3bd
Tea Antila <teaa@uef.fi>
2020-08-20 13:46:18 +0300

    Cypress

--- osa5/bloglist-backend/controllers/blogs.js
+++ osa5/bloglist-backend/controllers/blogs.js
@@ -21,6 +21,7 @@ router.get('/:id', async (request, response) => {
 })
 
 router.delete('/:id', async (request, response) => {
+  
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
   if (!request.token || !decodedToken.id) {
