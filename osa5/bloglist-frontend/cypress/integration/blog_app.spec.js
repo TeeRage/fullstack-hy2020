cafed9c38b1d66fe35545e43638ef956cbca03b5
Tea Antila <teaa@uef.fi>
2020-08-20 15:44:20 +0300

    5.17, 5.18 ja 5.19: blogilistan end to end -testit

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,12 +1,79 @@
 /**
- * Testi, joka varmistaa, että sovellus näyttää oletusarvoisesti kirjautumislomakkeen.
+ * Cypress-testit E2E -testaukseen
  */
-describe('Blog ', function() {
-  it('front page can be opened', function() {
+describe('Blog app', function() {
+
+  beforeEach(function() {
+
+    //Tyhjennetään testitietokanta ennen testejä
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    //Luodaan uusi käyttäjä backendille
+    const user = {
+      name: 'Esko Mörkö',
+      username: 'mörkö',
+      password: 'admin'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
+    //Siirrytään aloitussivulle
     cy.visit('http://localhost:3000')
+  })
+
+  //5.17: testaa, että kirjautumislomake näkyy alussa
+  it('5.17: Login from is shown', function() {
     cy.contains('Blogs')
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  //5.18: testaa sekä onnistuneen että epäonnistuneen kirjautumisen.
+  describe('5.18: Login',function() {
+
+    it('succeeds with correct credentials', function() {
+      cy.contains('login')
+      cy.get('#username').type('mörkö')
+      cy.get('#password').type('admin')
+      cy.get('#login-button').click()
+      cy.contains('Esko Mörkö logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login')
+      cy.get('#username').type('esko')
+      cy.get('#password').type('xxx')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+    })
+  })
+
+  //Testataan uuden blogin luominen (kirjautuneelle käytätjälle)
+  describe('5.19: When logged in', function() {
+
+    //Kirjaudutaan sisälle ennen testejä
+    beforeEach(function() {
+      cy.contains('login')
+      cy.get('#username').type('mörkö')
+      cy.get('#password').type('admin')
+      cy.get('#login-button').click()
+    })
+
+    //Luodaan uusi blogi
+    it('A blog can be created', function() {
+      cy.contains('Create new blog').click()
+      cy.get('#title').type('Agathan blogi')
+      cy.get('#author').type('Agatha Christie')
+      cy.get('#url').type('www.testimaa.fi')
+      cy.contains('create').click()
+      cy.contains('A new blog')
+      cy.contains('added')
+
+      //Varmistetaan, että blogi tulee näkyviin
+      cy.contains('Blogs')
+      cy.contains('Agathan blogi')
+      cy.contains('Agatha Christie')
+      cy.contains('view')
+    })
   })
 })
--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -151,23 +151,23 @@ const App = () => {
       <div>
         username
         <input
+          id='username'
           type="text"
           value={username}
-          name="Username"
           onChange={({ target }) => setUsername(target.value)}
         />
       </div>
       <div>
         password
         <input
+          id='password'
           type="password"
           value={password}
-          name="Password"
           autoComplete="on"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id="login-button" type="submit">login</button>
     </form>
   )//loginForm
 
